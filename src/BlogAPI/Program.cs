daaef47169662d135de0257b850a9d6e7d2dda8b
Luis Costa <scluispaulo@gmail.com>
2025-09-27 09:58:37 -0300

    Add Swagger

--- src/BlogAPI/Program.cs
+++ src/BlogAPI/Program.cs
@@ -1,11 +1,22 @@
 using Microsoft.EntityFrameworkCore;
+using Microsoft.OpenApi.Models;
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddDbContext<BlogContext>(options =>
     options.UseSqlite(builder.Configuration.GetConnectionString("SqliteConnectionString")));
 
+// Swagger
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new OpenApiInfo { Title = "BlogApi", Version = "v1" });
+});
+
 var app = builder.Build();
+
+app.UseSwagger();
+app.UseSwaggerUI();
 
 app.MapGet("/", () => "Hello World!");
 
